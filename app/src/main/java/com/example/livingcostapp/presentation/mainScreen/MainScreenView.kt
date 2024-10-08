ac369e442dd9a287d92368d30c04ecba1d624828
PanG95 <95.dgolik@gmail.com>
2024-10-08 20:55:57 +0200

    add box to main Screen

--- app/src/main/java/com/example/livingcostapp/presentation/mainScreen/MainScreenView.kt
+++ app/src/main/java/com/example/livingcostapp/presentation/mainScreen/MainScreenView.kt
@@ -2,19 +2,26 @@ package com.example.livingcostapp.presentation.mainScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -28,6 +35,11 @@ import androidx.navigation.compose.rememberNavController
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreenView(navController: NavController) {
+
+    val expensesState = remember { mutableStateOf(0) }
+    val earningsState = remember { mutableStateOf(0) }
+    val savingsState = remember { mutableStateOf(0) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -62,13 +74,13 @@ fun MainScreenView(navController: NavController) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues) // Uwzględnij `paddingValues` aby uniknąć nachodzenia na inne elementy
+                    .padding(paddingValues)
                     .padding(16.dp)
             ) {
                 // Nagłówek
                 Text(
                     text = "Ekran Główny",
-                    fontSize = 24.sp,
+                    fontSize = 36.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center,
                     modifier = Modifier
@@ -80,45 +92,82 @@ fun MainScreenView(navController: NavController) {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(100.dp)
+                        .height(200.dp)
                         .padding(bottom = 16.dp)
                         .background(Color.LightGray)
                         .clickable {
                             navController.navigate("expenses")
+                            expensesState.value += 50
                         },
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = "Wydatki", fontSize = 18.sp)
+                    Text("Wydatki: ${expensesState.value} PLN", fontSize = 30.sp)
                 }
 
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(100.dp)
+                        .height(200.dp)
                         .padding(bottom = 16.dp)
                         .background(Color.LightGray)
                         .clickable {
                             navController.navigate("earnings")
+                            earningsState.value += 100
                         },
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = "Przychody", fontSize = 18.sp)
+                    Text("Przychodz: ${earningsState.value}", fontSize = 30.sp)
                 }
 
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(100.dp)
+                        .height(200.dp)
                         .background(Color.LightGray)
                         .clickable {
                             navController.navigate("savings")
+                            savingsState.value += 200
                         },
                     contentAlignment = Alignment.Center
+
                 ) {
-                    Text(text = "Oszczędności", fontSize = 18.sp)
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.Top
+                    )
+
+                    {
+                        Text(
+                            text = "Oszczędności",
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(100.dp)
+                                .background(
+                                    Color.Blue,
+                                    shape = RoundedCornerShape(16.dp)
+                                )
+                                .align(Alignment.CenterHorizontally)
+                                .padding(9.dp)
+                        ) {
+                            Text(
+                                text = "${savingsState.value}",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                modifier = Modifier
+                                    .align(Alignment.Center)
+
+                            )
+                        }
+                    }
                 }
             }
         })
+
 }
 
 
